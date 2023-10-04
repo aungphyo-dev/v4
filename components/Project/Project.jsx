39cc5ca73d332a39aca7fc0bde2600bae41e8d41
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-10-04 19:51:40 +0630

    fix

--- components/Project/Project.jsx
+++ components/Project/Project.jsx
@@ -1,9 +1,16 @@
 import './project.css'
 import {BsArrowRightShort} from "react-icons/bs";
 import ProjectCard from "@/components/Project_Card/ProjectCard";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 
 const Project = ({projects}) => {
+    const router = useRouter()
+    useEffect(() => {
+        router.prefetch("/projects")
+    }, []);
     return (
         <div data-section className=' pt-[25px]  lg:pt-[35px]' id='projects'>
             <div className="sticky top-0 z-20 mb-4 w-full bg-slate-900/75 py-5 backdrop-blur lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"><h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Projects</h2></div>
@@ -11,10 +18,10 @@ const Project = ({projects}) => {
                 {projects?.map(project=><ProjectCard key={project.id} project={project}/> )}
             </ul>
             <div>
-                <div  className='text-slate-200 hover:underline group'>
+                <Link href={"/projects"}  className='text-slate-200 hover:underline group'>
                     view all my projects
                     <BsArrowRightShort className=' opacity-0 transition duration-500 ml-1 inline-flex group-hover:opacity-100'/>
-                </div>
+                </Link>
             </div>
         </div>
 )
--- components/Project_Card/ProjectCard.jsx
+++ components/Project_Card/ProjectCard.jsx
@@ -2,36 +2,34 @@ import Link from "next/link";
 
 const ProjectCard = ({project}) => {
     return(
-            <li className="mb-12">
+        <li className="mb-12">
+            <Link href={project?.demo} target={"_blank"}
+                  className="group cursor-pointer relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:opacity-100">
                 <div
-                    className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:opacity-100">
-                    <div
-                        className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                    <div className="z-10 sm:order-2 sm:col-span-6">
-                        <h3
-                            className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base">
+                    className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                <div className="z-10 sm:order-2 sm:col-span-6">
+                    <h3
+                        className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base">
                                 <span
-                            className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                                <span>
+                                    className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                        <span>
                                     {project?.title}
                                 </span>
-                        </h3>
-                        <p className="mt-2 text-sm leading-normal text-slate-300">{project.description}</p>
-                        <ul className="mt-2 flex flex-wrap">
-                            {project.skills.map(skill=><li key={skill} className="mr-1.5 mt-2">
-                                <div
-                                    className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">{skill}
-                                </div>
-                            </li>)}
-                        </ul>
-                    </div>
-                    <Link  href={project?.demo} target={"_blank"}>
-                        <img alt="img" loading="lazy"
-                             src={`https://otgegesmjkdjmcppbsbl.supabase.co/storage/v1/object/public/projects/images/${project.image}`}
-                        />
-                    </Link>
-                    </div>
-            </li>
+                    </h3>
+                    <p className="mt-2 text-sm leading-normal text-slate-300">{project.description}</p>
+                    <ul className="mt-2 flex flex-wrap">
+                        {project.skills.map(skill=><li key={skill} className="mr-1.5 mt-2">
+                            <div
+                                className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">{skill}
+                            </div>
+                        </li>)}
+                    </ul>
+                </div>
+                <img alt="img" loading="lazy"
+                     className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                     src={`https://otgegesmjkdjmcppbsbl.supabase.co/storage/v1/object/public/projects/images/${project.image}`}
+                /></Link>
+        </li>
     )
 }
 export default ProjectCard
