64174629272a42569977af8901d515c6a06ed3d3
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-10-04 12:27:15 +0630

    fav

--- DB/Experiences.js
+++ DB/Experiences.js
@@ -1,5 +1,6 @@
 const Experiences = [
   {
+    "id" : 1,
     "title" : "Frontend Developer",
     "description" : "I work at Freelance  in July 1, 2023 as Frontend developer.",
     "duration" : "JULY 1,2023 — AUGUST 30, 2023",
