29e8f5429d7aa633e97ee176f6c5a53a3bf55612
typle <aungpyaephyo1412@gmail.com>
2023-10-16 11:35:00 +0630

    exp change

--- DB/Experiences.js
+++ DB/Experiences.js
@@ -3,7 +3,7 @@ const Experiences = [
     "id" : 1,
     "title" : "Frontend Developer",
     "description" : "I work at Freelance  in July 1, 2023 as Frontend developer.",
-    "duration" : "JULY 1,2023 — AUGUST 30, 2023",
+    "duration" : "JULY 1 — AUGUST 30, 2023",
     "skills" : ["Javascript","Typescript","ReactJS","Redux","Tailwindcss"]
   }
 ]
