51585c28efdbf8510bfd269482ea40ba8e57fdaf
typle <aungpyaephyo1412@gmail.com>
2023-10-17 08:20:06 +0630

    exp crad c

--- components/Experience_card/ExperienceCard.jsx
+++ components/Experience_card/ExperienceCard.jsx
@@ -6,10 +6,10 @@ const ExperienceCard = ({experience}) => {
               <div
                   className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
               <header
-                  className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                  className="z-10 mb-2 mt-1 text-[10px] font-semibold uppercase tracking-wide text-slate-500 sm:col-span-3">
                   {experience.duration}
               </header>
-              <div className="z-10 sm:col-span-6">
+              <div className="z-10 sm:col-span-5">
                   <h3 className="font-medium leading-snug text-slate-200">
                       <div>
                           <div
