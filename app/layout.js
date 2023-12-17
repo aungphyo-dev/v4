eca7218cbf9f91297d80cee9366864801f0d6a73
typle <aungpyaephyo1412@gmail.com>
2023-12-18 04:13:30 +0630

    fix : metadata

--- app/layout.js
+++ app/layout.js
@@ -3,10 +3,38 @@ import {Analytics} from "@vercel/analytics/react";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { GeistSans } from 'geist/font/sans';
 export const metadata = {
-    title: 'Typle | Developer',
-    description: 'Generate with Next 13',
+    metadataBase: new URL('https://aungpyaephyoe.vercel.app'),
+    title: {
+        default: 'Typle | Developer',
+        template: '%s | Typle',
+    },
     icons :[{ rel: 'icon', url: "/fav.png" }],
-}
+    description: 'Developer, writer, and creator.',
+    openGraph: {
+        title: 'Typle',
+        description: 'Developer, writer, and creator.',
+        url: 'https://aungpyaephyoe.vercel.app',
+        siteName: 'Typle',
+        locale: 'en_US',
+        type: 'website',
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    },
+    twitter: {
+        title: 'Typle',
+        card: 'summary_large_image',
+    },
+};
+
 export default function RootLayout({children}) {
     return (
         <html lang="en">
